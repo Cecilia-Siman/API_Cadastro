31c302f115df25771f523c75766452a6a6434957
Cecilia Salema <ceciliasiman15@gmail.com>
2023-02-24 00:24:34 -0300

    feat: :sparkles: Add body schema validation

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
 import UserController from "../Controllers/UserController.js";
+import schemaValidator from "../Middlewares/schemaValidator.js";
+import userSchema from "../Schemas/userSchema.js";
 
 const router = Router();
 
-router.post("/newClient", UserController.insertUserController);
+router.post(
+  "/newClient",
+  schemaValidator.validateSchema(userSchema),
+  UserController.insertUserController
+);
 router.get("/findClient/:cpf", UserController.findeUserController);
 router.get("/listClients", UserController.listUsersController);
 
--- src/Schemas/userSchema.js
+++ src/Schemas/userSchema.js
@@ -1,12 +1,13 @@
 import Joi from "joi";
 
-const cpfPattern = "/^[0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2}$/";
-const datePattern = "/^[0-9]{2}[/]?[-]?[0-9]{2}[/]?[-]?[0-9]{4}$/";
-
 const userSchema = Joi.object({
   name: Joi.string().required(),
-  cpf: Joi.string().pattern(cpfPattern).required(),
-  birthday: Joi.string().pattern(datePattern).required(),
+  cpf: Joi.string()
+    .pattern(/^[0-9]{3}[.]?[0-9]{3}[.]?[0-9]{3}[-]?[0-9]{2}$/)
+    .required(),
+  birthday: Joi.string()
+    .pattern(/^[0-9]{2}[/]?[-]?[0-9]{2}[/]?[-]?[0-9]{4}$/)
+    .required(),
 });
 
 export default userSchema;
